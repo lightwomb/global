21b1ad864cfb607c71d922c2336bb3b88060f41d
Jared <jaredboria@gmail.com>
2012-05-04 01:44:50 -0400

    Adding index.html and index_init_root.php
    The index_init_root.php is based off of a template that includes an
    index_init file for root access functions and other helpful code.

---
+++ global/index_init_root.php
@@ -0,0 +1,7 @@
+<?php
+// Directory Tool:
+defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+define('SITE_ROOT', '.');
+?>
+<?php require_once('global/initialize/init_paths.php'); ?>
+<?php require_once('global/initialize/init_misc.php'); ?>
